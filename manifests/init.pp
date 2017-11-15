043bebde20dd4aba3a567c083d134122a560a3d5
Cliff Wells <cwells@focusvision.com>
2017-11-15 11:23:17 -0800

    debug

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,11 @@
 class pingdom {
   $definitions = lookup('pingdom', Hash)
 
-  notify { $definitions: }
-
   $defaults = {
-    pingdom_account  => Sensitive($definitions['account']),
-    pingdom_username => Sensitive($definitions['username']),
-    pingdom_password => Sensitive($definitions['password']),
-    pingdom_appkey   => $definitions['appkey']
+    'pingdom_account'  => Sensitive($definitions['account']),
+    'pingdom_username' => Sensitive($definitions['username']),
+    'pingdom_password' => Sensitive($definitions['password']),
+    'pingdom_appkey'   => $definitions['appkey']
   }
 
   create_resources('pingdom_user', $definitions['users'], $defaults)

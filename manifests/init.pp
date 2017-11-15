badb36fb5f37cf70fc8c3c2fe608801658fadcd2
Cliff Wells <cwells@focusvision.com>
2017-11-15 10:54:05 -0800

    debugging

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,8 @@ class pingdom {
 
   $definitions = lookup('pingdom', Hash)
 
+  notify { "${definitions}": }
+
   $defaults = {
     pingdom_account  => Sensitive($definitions['account']),
     pingdom_username => Sensitive($definitions['username']),

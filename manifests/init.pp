8803c89d8060e4ba170137ac01eac51bf76fc453
Cliff Wells <cwells@focusvision.com>
2017-11-16 22:08:27 -0800

    try again

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,24 @@
 class pingdom {
-    $defaults = {
-        'account_email' => hiera('pingdom::account_email'),
-        'user_email'    => hiera('pingdom::user_email'),
-        'password'      => hiera('pingdom::password'),
-        'appkey'        => hiera('pingdom::appkey')
-    }
+    $account_email = hiera('pingdom::account_email'),
+    $user_email    = hiera('pingdom::user_email'),
+    $password      = hiera('pingdom::password'),
+    $appkey        = hiera('pingdom::appkey')
 
     Pingdom_user {
-        account_email => $defaults['account_email'],
-        user_email    => $defaults['user_email'],
-        password      => $defaults['password'],
-        appkey        => $defaults['appkey']
+        account_email => $account_email,
+        user_email    => $user_email,
+        password      => $password,
+        appkey        => $appkey
     }
 
     Pingdom_check {
-        account_email => $defaults['account_email'],
-        user_email    => $defaults['user_email'],
-        password      => $defaults['password'],
-        appkey        => $defaults['appkey']
+        account_email => $account_email,
+        user_email    => $user_email,
+        password      => $password,
+        appkey        => $appkey
     }
 
-    create_resources('pingdom_user',  hiera_hash('pingdom::users'),  $defaults)
-    create_resources('pingdom_check', hiera_hash('pingdom::checks'), $defaults)
+    create_resources('pingdom_user',  hiera_hash('pingdom::users'))
+    create_resources('pingdom_check', hiera_hash('pingdom::checks'))
 }
 

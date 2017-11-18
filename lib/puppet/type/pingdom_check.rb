ae4514f66e40ba90324a1c73b4f25ec561d4125d
Cliff Wells <cwells@focusvision.com>
2017-11-18 13:12:20 -0800

    ...

--- lib/puppet/type/pingdom_check.rb
+++ lib/puppet/type/pingdom_check.rb
@@ -178,7 +178,7 @@ Puppet::Type.newtype(:pingdom_check) do
         end
     end
 
-    newproperty(:user_contacts, :array_matching=>:all) do
+    newproperty(:users, :array_matching=>:all) do
         desc 'User names [list of strings].'
 
         def insync?(is)

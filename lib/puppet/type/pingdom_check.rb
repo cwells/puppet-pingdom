d6662323e527bff69c82cc36f64a4711becd36a2
Cliff Wells <cwells@focusvision.com>
2017-11-18 13:01:34 -0800

    ...

--- lib/puppet/type/pingdom_check.rb
+++ lib/puppet/type/pingdom_check.rb
@@ -298,7 +298,7 @@ Puppet::Type.newtype(:pingdom_check) do
     # autorequires
     #
     autorequire(:pingdom_user) do
-        self[:users]
+        self[:user_contacts]
     end
 
     autorequire(:pingdom_team) do

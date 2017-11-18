aecd778c482d7d8e84352198310431b60b4be114
Cliff Wells <cwells@focusvision.com>
2017-11-18 12:23:39 -0800

    ...

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,24 @@
 class pingdom {
-    $defaults = {
-        'account_email' => lookup('pingdom::account_email', String),
+    $account_email => lookup('pingdom::account_email', String, 'hash', false),
+
+    $common = {
         'user_email'    => lookup('pingdom::user_email', String),
         'password'      => lookup('pingdom::password', String),
         'appkey'        => lookup('pingdom::appkey', String)
-#        'log_level'     => lookup('pingdom::log_level')
+        'log_level'     => lookup('pingdom::log_level', String)
+    }
+
+    $defaults = $account_email ? {
+        default => merge($common, {'account_email' => $account_email})
+        false   => $common
     }
 
     $users = lookup('pingdom::users', Hash, 'hash', {})
     $teams = lookup('pingdom::teams', Hash, 'hash', {})
     $checks = lookup('pingdom::checks', Hash, 'hash', {})
 
-    notify { "${users}": }
-    notify { "${teams}": }
-    notify { "${checks}": }
-
-    # create_resources('pingdom_user', $users, $defaults)
-    # create_resources('pingdom_team', $teams, $defaults)
-    # create_resources('pingdom_check', $checks, $defaults)
+    create_resources('pingdom_user', $users, $defaults)
+    create_resources('pingdom_team', $teams, $defaults)
+    create_resources('pingdom_check', $checks, $defaults)
 }
 

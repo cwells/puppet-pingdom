5ef15955919a42d3727c26be2992e83db309fcd5
Cliff Wells <cwells@focusvision.com>
2017-11-19 11:03:19 -0800

    fml

--- lib/puppet/provider/pingdom_check/http.rb
+++ lib/puppet/provider/pingdom_check/http.rb
@@ -126,15 +126,16 @@ Puppet::Type.type(:pingdom_check).provide(:http) do
     end
 
     def exists?
-        if [:true, :bootstrap].include? @resource[:autofilter]
-            @autotag ||= 'puppet-' + Digest::SHA1.hexdigest(@resource[:name])[0..5]
-            @resource[:filter_tags] = [@autotag] if @resource[:autofilter] != :bootstrap
-            @property_hash[:tags] = [@autotag]
-        else
-            @autotag = nil
-        end
+        # if [:true, :bootstrap].include? @resource[:autofilter]
+        #     @autotag ||= 'puppet-' + Digest::SHA1.hexdigest(@resource[:name])[0..5]
+        #     @resource[:filter_tags] = [@autotag] if @resource[:autofilter] != :bootstrap
+        #     @property_hash[:tags] = [@autotag]
+        # else
+        #     @autotag = nil
+        # end
 
-        @check ||= api.find_check @resource[:name], @resource[:filter_tags]
+        # @check ||= api.find_check @resource[:name], @resource[:filter_tags]
+        @check = nil
     end
 
     def create
@@ -142,23 +143,23 @@ Puppet::Type.type(:pingdom_check).provide(:http) do
     end
 
     def flush
-        if @resource[:ensure] == :absent
-            api.delete_check @check if @check
-            return
-        end
+        # if @resource[:ensure] == :absent
+        #     api.delete_check @check if @check
+        #     return
+        # end
 
-        @resource.eachproperty do |prop|
-            prop = prop.to_s.to_sym
-            self.method("#{prop}=").call @resource[prop] if prop != :ensure
-        end
-        @property_hash[:name] = @resource[:name]
+        # @resource.eachproperty do |prop|
+        #     prop = prop.to_s.to_sym
+        #     self.method("#{prop}=").call @resource[prop] if prop != :ensure
+        # end
+        # @property_hash[:name] = @resource[:name]
 
-        if @check
-            api.modify_check @check, @property_hash
-        else
-            @property_hash[:type] = @resource[:provider]
-            api.create_check @property_hash
-        end
+        # if @check
+        #     api.modify_check @check, @property_hash
+        # else
+        #     @property_hash[:type] = @resource[:provider]
+        #     api.create_check @property_hash
+        # end
     end
 
     def destroy

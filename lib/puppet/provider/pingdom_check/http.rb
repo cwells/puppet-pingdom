e70190e2fb68d135770648d1cc1d05b3f91504c9
Cliff Wells <cwells@focusvision.com>
2017-11-19 13:43:24 -0800

    fml

--- lib/puppet/provider/pingdom_check/http.rb
+++ lib/puppet/provider/pingdom_check/http.rb
@@ -169,7 +169,8 @@ Puppet::Type.type(:pingdom_check).provide(:http) do
     end
 
     def tags
-        usertags = @check.fetch('tags', []).map { |tag| tag['name'] if tag['type'] == 'u' }
+        puts "TAGS #{@check}"
+        # usertags = @check.fetch('tags', []).map { |tag| tag['name'] if tag['type'] == 'u' }
         # usertags.delete @autotag
         # puts "TAGS: #{usertags}"
         # usertags

1fcbde64c2e3b1844cd0e23e2b4a58c933db958c
Cliff Wells <cwells@focusvision.com>
2017-11-19 11:59:18 -0800

    fml

--- lib/puppet/provider/pingdom_check/http.rb
+++ lib/puppet/provider/pingdom_check/http.rb
@@ -84,5 +84,24 @@ Puppet::Type.type(:pingdom_check).provide(:http, :parent => :check_base) do
         end
     end
 
+    def users
+        # accepts list of ids, returns list of names
+        ids = @check.fetch('userids', nil)
+        user = api.select_users(ids, search='id') if ids
+        if user.respond_to? :map
+            user.map { |u| u['name'] }
+        else
+            :absent
+        end
+    end
+
+    def users=(value)
+        # accepts list of names, returns list of ids
+        found = api.select_users(value, search='name')
+        raise 'Unknown user in list' unless found.size == value.size
+        ids = found.map { |u| u['id'] }
+        @property_hash[:userids] = ids
+    end
+
     accessorize
 end

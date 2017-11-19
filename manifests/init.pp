d34d351999f8cd392d3ac97bb79ef89128be593b
Cliff Wells <cwells@focusvision.com>
2017-11-19 00:39:27 -0800

    fml?

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,7 +27,7 @@ class pingdom {
     # }
 
     create_resources('pingdom_user', $users, $defaults)
-    # create_resources('pingdom_team', $teams, $defaults)
+    create_resources('pingdom_team', $teams, $defaults)
     # create_resources('pingdom_check', $checks, $defaults)
 }
 

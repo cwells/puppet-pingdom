1fab49f2085c0e4092c28a4439b62d7a26a463dc
Cliff Wells <cwells@focusvision.com>
2017-11-19 13:28:41 -0800

    fml

--- lib/puppet/provider/pingdom_check/http.rb
+++ lib/puppet/provider/pingdom_check/http.rb
@@ -176,7 +176,7 @@ Puppet::Type.type(:pingdom_check).provide(:http) do
 
     def tags=(value)
         puts "tags= #{@property_hash[:tags]} #{value}"
-        @property_hash[:tags] = @property_hash[:tags] + value
+        @property_hash[:tags] = (@property_hash[:tags] + value).join ','
     end
 
     def teams

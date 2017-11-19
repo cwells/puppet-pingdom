3132192a391e1d49bd391bcb49dade6d9cc3df02
Cliff Wells <cwells@focusvision.com>
2017-11-19 12:17:38 -0800

    fml

--- lib/puppet/provider/pingdom_check/check_base.rb
+++ lib/puppet/provider/pingdom_check/check_base.rb
@@ -30,29 +30,29 @@ end
 Puppet::Type.type(:pingdom_check).provide(:check_base) do
     confine :true => has_pingdom_api
 
-    def api
-        @api ||= begin
-            if @resource[:credentials_file]
-                require 'yaml'
-                # just let any exception bubble up
-                creds = YAML.load_file(
-                    File.expand_path @resource[:credentials_file]
-                )
-                account_email, user_email, password, appkey =
-                    creds['account_email'], creds['user_email'], creds['password'], creds['appkey']
-            else
-                raise 'Missing API credentials' if [
-                    @resource[:account_email],
-                    @resource[:user_email],
-                    @resource[:password],
-                    @resource[:appkey]
-                ].include? nil and @resource[:credentials_file].nil?
-                account_email, user_email, password, appkey =
-                    @resource[:account_email], @resource[:user_email], @resource[:password], @resource[:appkey]
-            end
-            PuppetX::Pingdom::Client.new(account_email, user_email, password, appkey, @resource[:log_level])
-        end
-    end
+    # def api
+    #     @api ||= begin
+    #         if @resource[:credentials_file]
+    #             require 'yaml'
+    #             # just let any exception bubble up
+    #             creds = YAML.load_file(
+    #                 File.expand_path @resource[:credentials_file]
+    #             )
+    #             account_email, user_email, password, appkey =
+    #                 creds['account_email'], creds['user_email'], creds['password'], creds['appkey']
+    #         else
+    #             raise 'Missing API credentials' if [
+    #                 @resource[:account_email],
+    #                 @resource[:user_email],
+    #                 @resource[:password],
+    #                 @resource[:appkey]
+    #             ].include? nil and @resource[:credentials_file].nil?
+    #             account_email, user_email, password, appkey =
+    #                 @resource[:account_email], @resource[:user_email], @resource[:password], @resource[:appkey]
+    #         end
+    #         PuppetX::Pingdom::Client.new(account_email, user_email, password, appkey, @resource[:log_level])
+    #     end
+    # end
 
     # def exists?
     #     if [:true, :bootstrap].include? @resource[:autofilter]
@@ -98,37 +98,37 @@ Puppet::Type.type(:pingdom_check).provide(:check_base) do
     # custom getters/setters
     #
 
-    #
-    # utility methods
-    #
-    def self.accessorize
-        # Provides automatic creation of missing getters/setters (accessors).
-        #
-        # Similar to mk_resource_methods, but doesn't clobber existing methods, thank you.
-        # This allows us to have special cases explicitly defined, while still benefitting
-        # from accessor auto-creation (which this class method provides).
-        # Should be called at the end of every provider definition (unless you explicitly
-        # define every single getter/setter).
+    # #
+    # # utility methods
+    # #
+    # def self.accessorize
+    #     # Provides automatic creation of missing getters/setters (accessors).
+    #     #
+    #     # Similar to mk_resource_methods, but doesn't clobber existing methods, thank you.
+    #     # This allows us to have special cases explicitly defined, while still benefitting
+    #     # from accessor auto-creation (which this class method provides).
+    #     # Should be called at the end of every provider definition (unless you explicitly
+    #     # define every single getter/setter).
 
-        [ resource_type.validproperties, resource_type.parameters ].flatten.each do |prop|
-            # It should be noted that this loops over all properties for all check providers.
-            # This is unfortunate, but we are protected against invalid properties by the
-            # `required_features` defined on each property in the type declarations.
-            prop = prop.to_sym
-            next if prop == :name
+    #     [ resource_type.validproperties, resource_type.parameters ].flatten.each do |prop|
+    #         # It should be noted that this loops over all properties for all check providers.
+    #         # This is unfortunate, but we are protected against invalid properties by the
+    #         # `required_features` defined on each property in the type declarations.
+    #         prop = prop.to_sym
+    #         next if prop == :name
 
-            if !method_defined?(prop)
-                define_method(prop) do
-                    @check.fetch(prop.to_s, :absent)
-                end
-            end
+    #         if !method_defined?(prop)
+    #             define_method(prop) do
+    #                 @check.fetch(prop.to_s, :absent)
+    #             end
+    #         end
 
-            setter = "#{prop}=".to_sym
-            if !method_defined?(setter)
-                define_method(setter) do |value|
-                    @property_hash[prop] = value
-                end
-            end
-        end
-    end
+    #         setter = "#{prop}=".to_sym
+    #         if !method_defined?(setter)
+    #             define_method(setter) do |value|
+    #                 @property_hash[prop] = value
+    #             end
+    #         end
+    #     end
+    # end
 end
--- lib/puppet/provider/pingdom_check/http.rb
+++ lib/puppet/provider/pingdom_check/http.rb
@@ -6,6 +6,30 @@ Puppet::Type.type(:pingdom_check).provide(:http, :parent => :check_base) do
                  :shouldnotcontain, :postdata, :requestheaders
     defaultfor :feature => :posix
 
+    def api
+        @api ||= begin
+            if @resource[:credentials_file]
+                require 'yaml'
+                # just let any exception bubble up
+                creds = YAML.load_file(
+                    File.expand_path @resource[:credentials_file]
+                )
+                account_email, user_email, password, appkey =
+                    creds['account_email'], creds['user_email'], creds['password'], creds['appkey']
+            else
+                raise 'Missing API credentials' if [
+                    @resource[:account_email],
+                    @resource[:user_email],
+                    @resource[:password],
+                    @resource[:appkey]
+                ].include? nil and @resource[:credentials_file].nil?
+                account_email, user_email, password, appkey =
+                    @resource[:account_email], @resource[:user_email], @resource[:password], @resource[:appkey]
+            end
+            PuppetX::Pingdom::Client.new(account_email, user_email, password, appkey, @resource[:log_level])
+        end
+    end
+
     def exists?
         if [:true, :bootstrap].include? @resource[:autofilter]
             @autotag ||= 'puppet-' + Digest::SHA1.hexdigest(@resource[:name])[0..5]
@@ -193,6 +217,40 @@ Puppet::Type.type(:pingdom_check).provide(:http, :parent => :check_base) do
         # ids = found.map { |u| u['id'] }
         # puts "IDS: #{ids}"
         # @property_hash[:userids] = ids
+    end
+
+    #
+    # utility methods
+    #
+    def self.accessorize
+        # Provides automatic creation of missing getters/setters (accessors).
+        #
+        # Similar to mk_resource_methods, but doesn't clobber existing methods, thank you.
+        # This allows us to have special cases explicitly defined, while still benefitting
+        # from accessor auto-creation (which this class method provides).
+        # Should be called at the end of every provider definition (unless you explicitly
+        # define every single getter/setter).
+
+        [ resource_type.validproperties, resource_type.parameters ].flatten.each do |prop|
+            # It should be noted that this loops over all properties for all check providers.
+            # This is unfortunate, but we are protected against invalid properties by the
+            # `required_features` defined on each property in the type declarations.
+            prop = prop.to_sym
+            next if prop == :name
+
+            if !method_defined?(prop)
+                define_method(prop) do
+                    @check.fetch(prop.to_s, :absent)
+                end
+            end
+
+            setter = "#{prop}=".to_sym
+            if !method_defined?(setter)
+                define_method(setter) do |value|
+                    @property_hash[prop] = value
+                end
+            end
+        end
     end
 
     accessorize

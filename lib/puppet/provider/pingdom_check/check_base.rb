6c2d5ad2a3948cb080fbc8b6e53ec2a4c55ba8b9
Cliff Wells <cwells@focusvision.com>
2017-11-19 11:48:29 -0800

    fml

--- lib/puppet/provider/pingdom_check/check_base.rb
+++ lib/puppet/provider/pingdom_check/check_base.rb
@@ -136,12 +136,12 @@ Puppet::Type.type(:pingdom_check).provide(:check_base) do
     end
 
     def teams=(value)
-        # accepts list of names, returns list of ids
-        teams = api.select_teams(value, search='name')
-        raise 'Unknown team in list' unless teams.size == value.size
-        ids = teams.map { |u| u['id'] }
-        # newvalue = ids.join(',') if ids.respond_to? :join
-        @property_hash[:teamids] = ids
+        # # accepts list of names, returns list of ids
+        # teams = api.select_teams(value, search='name')
+        # raise 'Unknown team in list' unless teams.size == value.size
+        # ids = teams.map { |u| u['id'] }
+        # # newvalue = ids.join(',') if ids.respond_to? :join
+        # # @property_hash[:teamids] = ids
     end
 
     def users
@@ -156,13 +156,13 @@ Puppet::Type.type(:pingdom_check).provide(:check_base) do
     end
 
     def users=(value)
-        puts "USERS=#{value}"
-        # accepts list of names, returns list of ids
-        found = api.select_users(value, search='name')
-        raise 'Unknown user in list' unless found.size == value.size
-        ids = found.map { |u| u['id'] }
-        # newvalue = ids.join(',') if ids.respond_to? :join
-        # @property_hash[:userids] = ids
+        # puts "USERS=#{value}"
+        # # accepts list of names, returns list of ids
+        # found = api.select_users(value, search='name')
+        # raise 'Unknown user in list' unless found.size == value.size
+        # ids = found.map { |u| u['id'] }
+        # # newvalue = ids.join(',') if ids.respond_to? :join
+        # # @property_hash[:userids] = ids
     end
 
     #

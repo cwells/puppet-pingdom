ebd3572ab3062985643be65069757a832c68ded1
Cliff Wells <cwells@focusvision.com>
2017-11-19 00:34:27 -0800

    go baby

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,14 +16,8 @@ class pingdom {
     $teams = lookup('pingdom::teams', Hash, 'hash', {})
     $checks = lookup('pingdom::checks', Hash, 'hash', {})
 
-    pingdom_user { 'SRE PagerDuty':
-        contact_targets => {
-            email => 'sre@focusvision.com'
-        }
-    }
-
-    # create_resources('pingdom_user', $users, $defaults)
+    create_resources('pingdom_user', $users, $defaults)
     create_resources('pingdom_team', $teams, $defaults)
-    # create_resources('pingdom_check', $checks, $defaults)
+    create_resources('pingdom_check', $checks, $defaults)
 }
 

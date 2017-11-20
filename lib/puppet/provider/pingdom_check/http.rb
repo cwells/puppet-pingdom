a39f99697a4f0e2a72d42cccebd68c5c0848d6eb
Cliff Wells <cwells@focusvision.com>
2017-11-19 16:39:10 -0800

    fuck puppet

--- lib/puppet/provider/pingdom_check/http.rb
+++ lib/puppet/provider/pingdom_check/http.rb
@@ -216,23 +216,22 @@ Puppet::Type.type(:pingdom_check).provide(:http) do
 
     def users
         # accepts list of ids, returns list of names
-        # ids = @check.fetch('userids', nil)
-        # user = api.select_users(ids, search='id') if ids
-        # if user.respond_to? :map
-        #     user.map { |u| u['name'] }
-        # else
-        #     :absent
-        # end
-        []
+        ids = @check.fetch('userids', nil)
+        user = api.select_users(ids, search='id') if ids
+        if user.respond_to? :map
+            user.map { |u| u['name'] }
+        else
+            :absent
+        end
     end
 
     def users=(value)
         # accepts list of names, returns list of ids
-        # found = api.select_users(value, search='name')
-        # raise 'Unknown user in list' unless found.size == value.size
-        # ids = found.map { |u| u['id'] }
-        # puts "IDS: #{ids}"
-        # @property_hash[:userids] = ids.join ','
+        found = api.select_users(value, search='name')
+        raise 'Unknown user in list' unless found.size == value.size
+        ids = found.map { |u| u['id'] }
+        puts "IDS: #{ids}"
+        @property_hash[:userids] = ids.join ','
     end
 
     #

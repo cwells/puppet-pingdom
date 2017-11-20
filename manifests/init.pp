85c29a78b8aa70e397c469a1e874a54d98c8e02e
Cliff Wells <cwells@focusvision.com>
2017-11-19 20:27:52 -0800

    fuck puppet

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,57 +1,60 @@
 class pingdom {
-    # $account_email = lookup('pingdom::account_email', String, 'first', false)
+    $account_email = lookup('pingdom::account_email', String, 'first', false)
 
-    # $common = {
-    #     'user_email' => lookup('pingdom::user_email', String, 'first'),
-    #     'password'   => lookup('pingdom::password', String, 'first'),
-    #     'appkey'     => lookup('pingdom::appkey', String, 'first')
+    $common = {
+        'user_email' => lookup('pingdom::user_email', String, 'first'),
+        'password'   => lookup('pingdom::password', String, 'first'),
+        'appkey'     => lookup('pingdom::appkey', String, 'first')
+    }
+
+    $defaults = $account_email ? {
+        default => merge($common, {'account_email' => $account_email}),
+        false   => $common
+    }
+
+    $users = lookup('pingdom::users', Hash, 'hash', {})
+    $teams = lookup('pingdom::teams', Hash, 'hash', {})
+    $checks = lookup('pingdom::checks', Hash, 'hash', {})
+
+    notify { "${account_email}": }
+    notify { "${common}": }
+
+    # pingdom_user { 'SRE PagerDuty':
+    #     ensure        => present,
+    #     # credentials_file => '~/.pingdom_credentials',
+    #     account_email => $account_email,
+    #     user_email    => $common['user_email'],
+    #     password      => $common['password'],
+    #     appkey        => $common['appkey'],
+    #     contact_targets => {
+    #         email => 'sre@focusvision.com'
+    #     }
     # }
 
-    # $defaults = $account_email ? {
-    #     default => merge($common, {'account_email' => $account_email}),
-    #     false   => $common
+    # pingdom_team { 'SRE':
+    #     ensure        => present,
+    #     # credentials_file => '~/.pingdom_credentials',
+    #     account_email => $account_email,
+    #     user_email    => $common['user_email'],
+    #     password      => $common['password'],
+    #     appkey        => $common['appkey'],
+    #     users         => ['SRE PagerDuty']
     # }
 
-    # $users = lookup('pingdom::users', Hash, 'hash', {})
-    # $teams = lookup('pingdom::teams', Hash, 'hash', {})
-    # $checks = lookup('pingdom::checks', Hash, 'hash', {})
-
-    pingdom_user { 'SRE PagerDuty':
-        ensure        => present,
-        credentials_file => '~/.pingdom_credentials',
-        # account_email => $account_email,
-        # user_email    => $common['user_email'],
-        # password      => $common['password'],
-        # appkey        => $common['appkey'],
-        contact_targets => {
-            email => 'sre@focusvision.com'
-        }
-    }
-
-    pingdom_team { 'SRE':
-        ensure        => present,
-        credentials_file => '~/.pingdom_credentials',
-        # account_email => $account_email,
-        # user_email    => $common['user_email'],
-        # password      => $common['password'],
-        # appkey        => $common['appkey'],
-        users         => ['SRE PagerDuty']
-    }
-
-    pingdom_check { "http://${facts['fqdn']}/check":
-        ensure        => present,
-        credentials_file => '~/.pingdom_credentials',
-        # account_email => $account_email,
-        # user_email    => $common['user_email'],
-        # password      => $common['password'],
-        # appkey        => $common['appkey'],
-        provider      => 'http',
-        host          => $facts['fqdn'],
-        url           => '/check',
-        tags          => ['http'],
-        teams         => ['SRE'],
-        users         => ['SRE PagerDuty']
-    }
+    # pingdom_check { "http://${facts['fqdn']}/check":
+    #     ensure        => present,
+    #     # credentials_file => '~/.pingdom_credentials',
+    #     account_email => $account_email,
+    #     user_email    => $common['user_email'],
+    #     password      => $common['password'],
+    #     appkey        => $common['appkey'],
+    #     provider      => 'http',
+    #     host          => $facts['fqdn'],
+    #     url           => '/check',
+    #     tags          => ['http'],
+    #     teams         => ['SRE'],
+    #     users         => ['SRE PagerDuty']
+    # }
 
     # create_resources('pingdom_user', $users, $defaults)
     # create_resources('pingdom_team', $teams, $defaults)

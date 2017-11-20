2b6ef1db3f4ec78a1ec38ef669b1bd6fbb66851b
Cliff Wells <cwells@focusvision.com>
2017-11-19 17:37:36 -0800

    fuck puppet

--- manifests/init.pp
+++ manifests/init.pp
@@ -47,7 +47,7 @@ class pingdom {
         url           => '/check',
         tags          => ['http'],
         contacts      => ['SRE PagerDuty'],
-        team_contacts        => ['SRE'],
+        teams         => ['SRE'],
         paused        => true
     }
 
